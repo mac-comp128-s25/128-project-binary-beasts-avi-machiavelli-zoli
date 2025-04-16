a5279cfa67bfbd74cb3ac46e7c9c68fd66ccf0c8
zlauko5 <zolblauk5@gmail.com>
2025-04-16 11:51:00 -0500

    wrote stuff in enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,7 +1,54 @@
-public class Enemy {
+import java.util.Random;
+import java.util.random.*;
+
+public class Enemy implements Character{
     private double health;
-    private double dmgMultiplier;
+    private double critMultiplier;
     private double critChance;
     private int priority;
     private double chanceToHit;
+    private double attackDamage;
+
+    public Enemy(double health, double critMultiplier, double critChance, int priority, double chanceToHit, double attackDamage){
+        this.health = health;
+        this.critMultiplier = critMultiplier;
+        this.critChance = critChance;
+        this.priority = priority;
+        this.chanceToHit = chanceToHit;
+        this.attackDamage = attackDamage;
+    }
+
+    public double getHealth(){
+        return health;
+    }
+
+    public void setHealth(double newHealth){
+        health = newHealth;
+    }
+
+    public boolean attack(){
+        if(checkIfHit(chanceToHit)){
+            // here do the actual setting of health and stuff
+
+            return true;
+        }
+        else {
+            System.out.println("The attack missed!"); 
+            return false;
+        }
+
+    }
+
+
+    // method decomp to check if an attack hits or not
+    private boolean checkIfHit(double chanceToHit){
+        Random rand = new Random();
+        double randomDouble = rand.nextDouble();
+        if(chanceToHit >= randomDouble){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
