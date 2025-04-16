673ff6e09f355e61f549a8b6df62a2dd28d12888
zlauko5 <zolblauk5@gmail.com>
2025-04-16 11:09:41 -0500

    enemy class

---
+++ src/Enemy.java
@@ -0,0 +1,7 @@
+public class Enemy {
+    private double health;
+    private double dmgMultiplier;
+    private double critChance;
+    private PriorityQueue priority;
+    private double chanceToHit;
+}
