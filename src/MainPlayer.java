20c0473d3729acd68248e2f46c0899aa7b1488e9
machiavellimerkleward <mmerklew@macalester.edu>
2025-04-18 11:24:55 -0500

    Changed MainPlayer and Character, made PriorityComparator

--- src/MainPlayer.java
+++ src/MainPlayer.java
@@ -1,36 +1,68 @@
+import java.util.Random;
+
 public class MainPlayer implements Character {
 
     private double health;
-    
     private double mana;
+    private String name;
+    private double critMultiplier;
+    private double critChance;
+    private int priority;
+    private double chanceToHit;
+    private double attackDamage;
+    private Random rand;
+
+    public MainPlayer(int health, int mana, String name, double critMultiplier, double critChance, int priority, double chanceToHit, double attackDamage){
+        this.health = health;
+        this.mana = mana;
+        this.name=  name;
+        this.critMultiplier = critMultiplier;
+        this.critChance = critChance;
+        this.priority = priority;
+        this.chanceToHit = chanceToHit;
+        this.attackDamage = attackDamage;
+        this.rand = new Random();
+    }
+
     @Override
     public boolean attack(Character target) {
-        // TODO Auto-generated method stub
+        if(!checkIfHit(chanceToHit)){
+            return false;
+        }
+        else{
+            
+        }
         throw new UnsupportedOperationException("Unimplemented method 'attack'");
+    }
+
+    private boolean checkIfHit(double chanceToHit){
+        double randomDouble = rand.nextDouble();
+        if(chanceToHit >= randomDouble){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     @Override
     public double getHealth() {
-        // TODO Auto-generated method stub
         return health;
     }
 
     @Override
     public void setHealth(double input) {
-        // TODO Auto-generated method stub
         health=input;
     }
 
     @Override
-    public int compareTo() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    public String toString(){
+        return name;
     }
 
     @Override
-    public boolean equals() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'equals'");
+    public double getPriority() {
+        return priority;
     }
 
     
